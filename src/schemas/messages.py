9d55b02160a7052b71692ff86de2b35196831389
Akeka_ <vladvaledol@gmail.com>
2025-05-15 11:27:49 +0300

    messages schema

---
+++ src/schemas/messages.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from pydantic import BaseModel
+
+
+class MessageAdd(BaseModel):
+    sender_id: int
+    recipient_id: int
+    content: str
+
+
+class Message(BaseModel):
+    id: int
+    sender_id: int
+    recipient_id: int
+    content: str
+    created_at: date
