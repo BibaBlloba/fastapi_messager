6943966f7d3261a24f6869781e9db2d74c897085
Akeka_ <vladvaledol@gmail.com>
2025-05-15 11:55:02 +0300

    auth depends

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,8 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends
+import jwt
+from fastapi import Depends, HTTPException, Request
 
+from services.auth import AuthService
 from src.database import async_session_maker
+from src.schemas.users import User
 from utils.db_manager import DbManager
 
 
@@ -12,3 +15,37 @@ async def get_db():
 
 
 DbDep = Annotated[DbManager, Depends(get_db)]
+
+
+async def get_current_token(request: Request):
+    token = request.cookies.get('access_token', None)
+    if token is None:
+        try:
+            credentials = request.headers['Authorization']
+            scheme, token = credentials.split()
+            if scheme.lower() != 'bearer':
+                raise HTTPException(401)
+        except KeyError:
+            raise HTTPException(401)
+    return token
+
+
+async def get_current_user(token: str = Depends(get_current_token)):
+    try:
+        decoded_data = AuthService().decode_token(token)
+        user = User(
+            id=decoded_data.get('id'),
+            login=decoded_data.get('login'),
+            username=decoded_data.get('username'),
+            created_at=decoded_data.get('created_at'),
+        )
+    except jwt.exceptions.DecodeError:
+        raise HTTPException(status_code=401, detail='Токен не действителен.')
+    except KeyError as e:
+        raise HTTPException(
+            status_code=401, detail=f'Недостаточно данных в токене: {str(e)}'
+        )
+    return user
+
+
+UserDap = Annotated[User, Depends(get_current_user)]
