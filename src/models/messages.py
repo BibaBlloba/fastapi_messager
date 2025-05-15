3e156a60d989d88ff3d88008f5025d862b30b490
Akeka_ <vladvaledol@gmail.com>
2025-05-15 14:34:54 +0300

    messages index

--- src/models/messages.py
+++ src/models/messages.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Index
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -14,3 +14,16 @@ class MessageOrm(Base):
     recipient_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     content: Mapped[str] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(default=datetime.now())
+
+    __table_args__ = (
+        (
+            Index(
+                'idx_message_recipient',
+                'recipient_id',
+            ),
+            Index(
+                'idx_message_sender',
+                'sender_id',
+            ),
+        ),
+    )
